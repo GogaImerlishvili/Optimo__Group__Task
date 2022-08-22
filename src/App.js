e11d994a9f1fae6e06c6922a767374c61f13c4d5
Goga <gogaimerlishvili1@gmail.com>
2022-08-22 19:29:08 +0400

    imported navbar comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Navbar />} />
+      </Routes>
+    </>
   );
 }
 
