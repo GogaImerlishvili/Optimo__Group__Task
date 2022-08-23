1bb442b9595f68bf5dcc40e682f04d94cb827555
Goga <gogaimerlishvili1@gmail.com>
2022-08-23 12:46:09 +0400

    imported employees

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Employees from "./components/Employees";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Navbar />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/employees" element={<Employees />} />
       </Routes>
     </>
   );
