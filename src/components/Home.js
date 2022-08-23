bed73703d836c547252a12b191930d29359dad0a
Goga <gogaimerlishvili1@gmail.com>
2022-08-23 12:47:54 +0400

    added homepage with navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Home.css";
-import video from "../assets/Video/video-1.mp4";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
 const Home = () => {
   return (
-    <div className="hero-container">
-      <video src={video} autoPlay loop muted />
-      <h1 className="title">We deliver Tomorrow's Solutions Today</h1>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   );
 };
 
