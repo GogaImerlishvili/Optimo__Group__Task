a36724b309651218bb23776a1e42e422efc392bd
Goga <gogaimerlishvili1@gmail.com>
2022-08-24 21:56:17 +0400

    added social icons data  for footer

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,14 @@
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+
+export const socialIcons = [
+  {
+    id: 2,
+    icon: <BsLinkedin />,
+    url: "https://www.linkedin.com/in/goga-imerlishvili-1984a622a/",
+  },
+  {
+    id: 3,
+    icon: <BsGithub />,
+    url: "https://github.com/GogaImerlishvili",
+  },
+];
