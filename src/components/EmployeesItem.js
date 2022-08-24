d4490ebecb5756e0233900fdc92f33ae94f015e6
Goga <gogaimerlishvili1@gmail.com>
2022-08-24 21:53:55 +0400

    get info from api for employees

--- src/components/EmployeesItem.js
+++ src/components/EmployeesItem.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./EmployeesItem.css";
 import { FcLike } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const EmployeesItem = (props) => {
+  const [singleItem, setSingleItem] = useState([]);
+  const [click, setClick] = useState(false);
+
+  const singleEmployee = () => {
+    const fetchSingleEmployee = async () => {
+      const response = await fetch(
+        `https://test-task-api-optimo.herokuapp.com/employee/${props.id}`
+      );
+
+      const responseData = await response.json();
+      setSingleItem(responseData);
+      console.log(responseData);
+    };
+    fetchSingleEmployee();
+  };
+
   return (
     <>
       <div className="employee-items">
-        <div className="avatar">
+        <div className="avatar" onClick={singleEmployee}>
           <div className="info">
             <img
+              key={props.id}
               src={"https://test-task-api-optimo.herokuapp.com" + props.avatar}
               alt="avatar"
             />
@@ -17,6 +35,9 @@ const EmployeesItem = (props) => {
             <span>
               <FcLike /> {props.liked}
             </span>
+            <Link className="details-button" to={`/employee/${props.id}`}>
+              Details
+            </Link>
           </div>
         </div>
       </div>
