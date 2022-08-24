b6c760a6cf1ee89d465a080cfacfe32d1c1fef5c
Goga <gogaimerlishvili1@gmail.com>
2022-08-24 21:52:04 +0400

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Employees from "./components/Employees";
+import Feedback from "./pages/Feedback";
+import Employee from "./components/Employee";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
@@ -11,8 +14,11 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/employees" element={<Employees />} />
+          <Route path="/employee" element={<Employee />} />
+          <Route path="/feedback" element={<Feedback />} />
         </Routes>
       </div>
+      <Footer />
     </>
   );
 }
