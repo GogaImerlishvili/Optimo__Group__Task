1f72fc098b81f7615f72dc8c103847627f2c0b56
Goga <gogaimerlishvili1@gmail.com>
2022-08-24 07:10:58 +0400

    get info from props for employees page

--- src/components/EmployeesItem.js
+++ src/components/EmployeesItem.js
@@ -1,19 +1,26 @@
 import React from "react";
+import "./EmployeesItem.css";
+import { FcLike } from "react-icons/fc";
 
 const EmployeesItem = (props) => {
   return (
-    <div className="employee-items">
-      <div className="avatar">
-        <div>
-          <img
-            src={"https://test-task-api-optimo.herokuapp.com" + props.avatar}
-            alt="avatar"
-          />
-          <h1>{props.name}</h1>
-          <p>{props.description}</p>
+    <>
+      <div className="employee-items">
+        <div className="avatar">
+          <div className="info">
+            <img
+              src={"https://test-task-api-optimo.herokuapp.com" + props.avatar}
+              alt="avatar"
+            />
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+            <span>
+              <FcLike /> {props.liked}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
